1ff33f05993c2a2c6a239c5e0317f97feeb56f18
hahalol47 <58111204+hahalol47@users.noreply.github.com>
2019-11-23 14:48:59 +0100

    Add files via upload

---
+++ javascriptCALCULATOR.js
@@ -0,0 +1,103 @@
+var screen= "15";
+
+function one() {
+    document.getElementById("outputId").innerHTML += "1";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function two() {
+    document.getElementById("outputId").innerHTML += "2";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+
+function three() {
+    document.getElementById("outputId").innerHTML += "3";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function four() {
+    document.getElementById("outputId").innerHTML += "4";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function five() {
+    document.getElementById("outputId").innerHTML += "5";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function six() {
+    document.getElementById("outputId").innerHTML += "6";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function seven() {
+    document.getElementById("outputId").innerHTML += "7";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function eight() {
+    document.getElementById("outputId").innerHTML += "8";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function nine() {
+    document.getElementById("outputId").innerHTML += "9";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function zero() {
+    document.getElementById("outputId").innerHTML = "0";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function addition() {
+    document.getElementById("outputId").innerHTML += "+";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function subtraction() {
+    document.getElementById("outputId").innerHTML += "-";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function division() {
+    document.getElementById("outputId").innerHTML += "/";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function multiplication() {
+    document.getElementById("outputId").innerHTML += "*";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function point() {
+    document.getElementById("outputId").innerHTML += ".";
+    document.getElementById("outputId").style.color = "white";
+    document.getElementById("outputId").style.fontSize = "40px";
+}
+
+function AC() {
+    document.getElementById("outputId").innerHTML = "";
+}
+
+function equals() {
+    document.getElementById("outputId").innerHTML = eval (document.getElementById("outputId").innerHTML)
+}
+
+
+
